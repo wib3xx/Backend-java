9e09d587c328501abeedae5f100cbd6d3fba0289
Sarvagay Sanmotra <sarvagay123gist@gmail.com>
2026-06-02 09:37:09 +0530

    First commit for Java backend course

---
+++ Day 2/HelloWorld.java
@@ -0,0 +1,42 @@
+class HelloWorld
+{ 
+
+// Method always perform the tasks(Work - printing odt, saying hello, booking ticket)
+public static void main(String [] args) //executing OR entry point
+{ 
+
+System.out.println("HelloWorld");
+
+}
+
+
+
+
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
