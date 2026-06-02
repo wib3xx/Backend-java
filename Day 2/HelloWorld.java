ee202cb5e802acc765862ad0a624f3b700b72cb2
Sarvagay Sanmotra <sarvagay123gist@gmail.com>
2026-06-02 22:29:22 +0530

    Updated HelloWorld.java

--- Day 2/HelloWorld.java
+++ Day 2/HelloWorld.java
@@ -1,14 +1,13 @@
 class HelloWorld
 { 
 
-// Method always perform the tasks(Work - printing odt, saying hello, booking ticket)
+// Method always perform the tasks(Work - printing dot, saying hello, booking ticket)
 public static void main(String [] args) //executing OR entry point
-{ 
+	{ 
 
-System.out.println("HelloWorld");
+		System.out.println("This is the first lecture of Java Backend course"); //main method
 
-}
-
+	}
 
 
 
