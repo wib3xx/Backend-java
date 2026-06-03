8b95485e32ae20696faf1dcc41d64e23afe9aae7
Sarvagay Sanmotra <sarvagay123gist@gmail.com>
2026-06-03 19:20:44 +0530

    New Practice Programs

--- Day 3/JavaBasics.java
+++ Day 3/JavaBasics.java
@@ -1,12 +1,14 @@
-class BackEnd{
+class BackEnd
 
-public static void main(String [] args)
+{   //class is a keyword //Convention of the object naming(Meaningful)
+
+public static void main(String [] args) //<<--------------Method (String is a class)(JVM initalizes here)
 
 	{
 
 	System.out.println("Exploring basic needs of Java");
 	System.out.println("Third day of the class");
-	System.out.println("Understanding the needs of the error and rectify");
+	System.out.println("Understanding the reason for the error and How to rectify it");
 
 	}
 
