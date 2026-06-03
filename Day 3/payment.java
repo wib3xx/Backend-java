029ad5d2c4802ee0cd4fcd39e8e7cfb8a051373c
Sarvagay Sanmotra <sarvagay123gist@gmail.com>
2026-06-03 23:27:25 +0530

    One more practice of Syntax

--- Day 3/payment.java
+++ Day 3/payment.java
@@ -5,6 +5,7 @@ public static void main(String [] args)
 	System.out.println("First Payment done");
 	
 	System.out.println("Second Payment done");
+	System.out.println("Third Payment Done");
 	
 	}
 }
