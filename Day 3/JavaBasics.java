9c80c51fc8f917edc5f6a29b74060ebf4a272246
Sarvagay Sanmotra <sarvagay123gist@gmail.com>
2026-06-03 09:04:34 +0530

    Added new Java practice concepts

---
+++ Day 3/JavaBasics.java
@@ -0,0 +1,14 @@
+class BackEnd{
+
+public static void main(String [] args)
+
+	{
+
+	System.out.println("Exploring basic needs of Java");
+	System.out.println("Third day of the class");
+	System.out.println("Understanding the needs of the error and rectify");
+
+	}
+
+
+}
