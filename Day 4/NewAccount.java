ba8efb31425a777eb8527297ca21f14ae846c8c8
Sarvagay Sanmotra <sarvagay123gist@gmail.com>
2026-06-04 22:35:25 +0530

    Practice for understanding String[] args concept

---
+++ Day 4/NewAccount.java
@@ -0,0 +1,13 @@
+class IstAccount{
+
+public static void main(String[] args){
+
+	String name1 = args[0];
+	String name2 = args[1];
+
+	int age1 = 24;
+	int age2 = 25;
+System.out.println("Account belongs to " + name1 + " and " + name2 + " with age: " + age1+" & " +age2+  " Respectively!!");
+
+	}	
+}
