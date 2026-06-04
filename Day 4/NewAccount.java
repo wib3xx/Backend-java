a0c0ea2d6fd0a8699336ac175079421bdd94cf09
Sarvagay Sanmotra <sarvagay123gist@gmail.com>
2026-06-04 23:18:03 +0530

    New commits for Practice using more arrays

--- Day 4/NewAccount.java
+++ Day 4/NewAccount.java
@@ -1,12 +1,12 @@
-class IstAccount{
+class IstAccount{  // Metaspace stores class information
 
 public static void main(String[] args){
 
-	String name1 = args[0];
-	String name2 = args[1];
+	String name1 = args[0];           //When main() starts:  All the data btwn {} goes into stack
+	String name2 = args[1];           //When main() starts:  All the data btwn {} goes into stack
 
-	int age1 = 24;
-	int age2 = 25;
+	int age1 = 24;                    //When main() starts:  All the data btwn {} goes into stack
+	int age2 = 25;                    //When main() starts:  All the data btwn {} goes into stack
 System.out.println("Account belongs to " + name1 + " and " + name2 + " with age: " + age1+" & " +age2+  " Respectively!!");
 
 	}	
