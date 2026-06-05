f3468a546c1599a4c8dfa19476022b9caddbd324
Sarvagay Sanmotra <sarvagay123gist@gmail.com>
2026-06-05 09:42:37 +0530

    Using String[] args to take as an output

---
+++ Day 5/NewEmployeeDetails.java
@@ -0,0 +1,28 @@
+class EmployeeDetails{
+
+	public static void main(String[] args){
+	
+	String name = args[0];
+	String lastName = args[1];
+	String eMail = args[2];
+	String stateName = args[3];
+	String CountName = args[4];
+	
+	System.out.println("Employee Details as follows: ");
+	System.out.println("First Name = " + name);
+	System.out.println("Last Name = " + lastName);
+	System.out.println("Email = " + eMail);
+	System.out.println("State = " + stateName);
+	System.out.println("Country  = " + CountName);
+	
+	
+	}
+	
+
+
+
+
+
+
+
+}
