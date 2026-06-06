b9d012098e074c286b25f9a186bcad6230b36832
Sarvagay Sanmotra <sarvagay123gist@gmail.com>
2026-06-06 17:56:47 +0530

    New Practice of yesterday: Updated

--- Day 5/NewEmployeeDetails.java
+++ Day 5/NewEmployeeDetails.java
@@ -7,6 +7,8 @@ class EmployeeDetails{
 	String eMail = args[2];
 	String stateName = args[3];
 	String CountName = args[4];
+	String StateName= args[5];
+	String Qual = args[6];
 	
 	System.out.println("Employee Details as follows: ");
 	System.out.println("First Name = " + name);
@@ -14,6 +16,8 @@ class EmployeeDetails{
 	System.out.println("Email = " + eMail);
 	System.out.println("State = " + stateName);
 	System.out.println("Country  = " + CountName);
+	System.out.println("Current City = " + StateName);
+	System.out.println("Qualifiction = " + Qual);
 	
 	
 	}
