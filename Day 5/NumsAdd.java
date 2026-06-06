a70e5cb77253340b90687421c3418e8cc84e5a9e
Sarvagay Sanmotra <sarvagay123gist@gmail.com>
2026-06-06 18:04:55 +0530

    Some of numbers --- Practice

--- Day 5/NumsAdd.java
+++ Day 5/NumsAdd.java
@@ -6,13 +6,17 @@ class AddNumbers{
 	
 	String firstNumberStr = args[0];
 	String secondNumberStr = args[1];
+	String thirdNumberStr = args[2];
+	String fourthNumberStr = args[3];
 	
 	//==> This convert the number ("1" format to 1 !)
 
 	int firstNumber = Integer.parseInt(firstNumberStr);
 	int secondNumber = Integer.parseInt(secondNumberStr);
+	int thirdNumber = Integer.parseInt(thirdNumberStr);
+	int fourthNumber = Integer.parseInt(fourthNumberStr);
 	
-	int sum = firstNumber + secondNumber;
+	int sum = firstNumber + secondNumber + thirdNumber + fourthNumber;
 	
 	System.out.println("Sum of the numbers is " + sum);
 	
