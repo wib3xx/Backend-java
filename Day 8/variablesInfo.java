d2baaa1480d9718168d2d2df2d2f1e6ad0ec5fd1
Sarvagay Sanmotra <sarvagay123gist@gmail.com>
2026-06-10 09:08:53 +0530

    New Concept Static Variable

--- Day 8/variablesInfo.java
+++ Day 8/variablesInfo.java
@@ -1,10 +1,13 @@
 class VariablesInfo
 {
+	int petrolPerLitre = 101;
+	
+	
 	public static void main(String[] args)
 	
 	{ //Method starts here
 	
-	// Local variables lies between Main Method{}
+	// Local variables lies between Main Method{}   //Stores in Stack
 	
 	int carPrice = 5000000;
 	System.out.println("Price of the car: "+ carPrice);
@@ -19,5 +22,8 @@ class VariablesInfo
 	System.out.println("Category of the car: "+ carCategory);
 	
 	} //Method ends here
-	
+	public  void bharatPetroleum()
+	{
+		System.out.println("Petrol/L = " + petrolPerLitre);
+	}
 }
