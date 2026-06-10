8b8ee8e092db119e27f0524eeb5e6d2855481048
Sarvagay Sanmotra <sarvagay123gist@gmail.com>
2026-06-10 08:33:06 +0530

    Variables - local variables: Explained

---
+++ Day 8/variablesInfo.java
@@ -0,0 +1,23 @@
+class VariablesInfo
+{
+	public static void main(String[] args)
+	
+	{ //Method starts here
+	
+	// Local variables lies between Main Method{}
+	
+	int carPrice = 5000000;
+	System.out.println("Price of the car: "+ carPrice);
+	
+	String carName = "Fortuner";
+	System.out.println("Car name: "+ carName);
+	
+	float carMileage = 28.05f;
+	System.out.println("Car's mileage: "+ carMileage);
+	
+	char carCategory = 'A';
+	System.out.println("Category of the car: "+ carCategory);
+	
+	} //Method ends here
+	
+}
