37b2ca569ec1a1adc8252f6e592bbbd684163579
Sarvagay Sanmotra <sarvagay123gist@gmail.com>
2026-06-12 18:18:23 +0530

    Lecture Day 10 added

---
+++ Day 10/Lecture/AddNumbers.java
@@ -0,0 +1,29 @@
+class AddNumbers{
+
+public static void main(String[] args){ //JVM is calling this main method(AddNumbers.main())
+
+	System.out.println("Start main()");
+	//call the methods from main()
+	
+	String input1 = args[0];
+	String input2 = args[1];
+	
+	int first = Integer.parseInt(input1);
+	int second = Integer.parseInt(input2);
+	
+	int result = AddNumbers.add(first , second);
+
+	System.out.println("Sum of two numbers: "+ result);
+	
+	System.out.println("End main()");
+	}
+
+static int add(int firstNumber, int secNumber){
+
+	System.out.println("Start add()");
+	int sum = firstNumber + secNumber;
+	System.out.println("End add()");
+	
+	return sum;
+	}
+}
