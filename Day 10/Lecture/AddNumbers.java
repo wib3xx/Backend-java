86ed3584b443a0873cb8fed7bc678f80315c74f0
Sarvagay Sanmotra <sarvagay123gist@gmail.com>
2026-06-17 10:50:51 +0530

    Accessors & Modifiers concept

--- Day 10/Lecture/AddNumbers.java
+++ Day 10/Lecture/AddNumbers.java
@@ -1,6 +1,9 @@
+// Add two numbers using two methods
+
+
 class AddNumbers{
 
-public static void main(String[] args){ //JVM is calling this main method(AddNumbers.main())
+public static void main(String[] args){ //JVM is calling this main method(AddNumbers.main())  //A static method calls another static method
 
 	System.out.println("Start main()");
 	//call the methods from main()
@@ -11,14 +14,14 @@ public static void main(String[] args){ //JVM is calling this main method(AddNum
 	int first = Integer.parseInt(input1);
 	int second = Integer.parseInt(input2);
 	
-	int result = AddNumbers.add(first , second);
+	int result = AddNumbers.add(first , second); //calling a method
 
 	System.out.println("Sum of two numbers: "+ result);
 	
 	System.out.println("End main()");
-	}
+	} 
 
-static int add(int firstNumber, int secNumber){
+static int add(int firstNumber, int secNumber){  //A static method calls another static method
 
 	System.out.println("Start add()");
 	int sum = firstNumber + secNumber;
