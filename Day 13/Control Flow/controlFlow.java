5b80cbf0f67c3cfba23b4fca72a521fa59ba872b
Sarvagay Sanmotra <sarvagay123gist@gmail.com>
2026-06-18 22:57:44 +0530

    Practice of control flow with method calling & access

--- Day 13/Control Flow/controlFlow.java
+++ Day 13/Control Flow/controlFlow.java
@@ -5,7 +5,7 @@ class controlFlow{
 	int amount = Integer.parseInt(args[0]);
 	
 	boolean deliveryStatus = controlFlow.checkDelStatus(amount);
-		System.out.println("You have placed your order successfully, You got "+ deliveryStatus);
+		System.out.println("Customer delivery status(true : free | false : paid)"+ deliveryStatus);
 	
 	}
 	private static boolean checkDelStatus(int amount){
@@ -13,10 +13,10 @@ class controlFlow{
 	boolean status = false;
 	
 	if(amount>= 10000){
-		System.out.println("Free Delivery");
+		System.out.println("Inside if block");
 		return true;
 		}else{
-			System.out.println("No free delivery");
+			System.out.println("Inside else block");
 		}
 			return status;
 	              // check free delivery if order value is 350 and above
