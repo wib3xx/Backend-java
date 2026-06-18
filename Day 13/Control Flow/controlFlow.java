c93ed434e4e8427c73c35e01bbdfadf98c6a9790
Sarvagay Sanmotra <sarvagay123gist@gmail.com>
2026-06-18 22:36:12 +0530

    Practice

--- Day 13/Control Flow/controlFlow.java
+++ Day 13/Control Flow/controlFlow.java
@@ -5,7 +5,7 @@ class controlFlow{
 	int amount = Integer.parseInt(args[0]);
 	
 	boolean deliveryStatus = controlFlow.checkDelStatus(amount);
-		System.out.println("Your delivery is Free or Paid = "+ deliveryStatus);
+		System.out.println("You have placed your order successfully, You got "+ deliveryStatus);
 	
 	}
 	private static boolean checkDelStatus(int amount){
@@ -13,7 +13,10 @@ class controlFlow{
 	boolean status = false;
 	
 	if(amount>= 10000){
+		System.out.println("Free Delivery");
 		return true;
+		}else{
+			System.out.println("No free delivery");
 		}
 			return status;
 	              // check free delivery if order value is 350 and above
