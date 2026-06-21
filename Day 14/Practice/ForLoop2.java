d6dc3fcc4dd1e743d592e7fa921fc3c966ac336c
Sarvagay Sanmotra <sarvagay123gist@gmail.com>
2026-06-21 23:55:10 +0530

    Lucky Number Programs

--- Day 14/Practice/ForLoop2.java
+++ Day 14/Practice/ForLoop2.java
@@ -2,7 +2,7 @@ class ForLoop2{
 
 	public static void main(String[] args){
 	
-		for(int i = 1 ; i<= 20 ; i++){
+		for(int i = 2 ; i<= 10 ; i++){
 			System.out.println("Java Backend Engineer");
 		
 		}
