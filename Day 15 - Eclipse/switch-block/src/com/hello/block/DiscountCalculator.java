3951e51d046e11c462f3a57c4a4ab95b67a95468
Sarvagay Sanmotra <sarvagay123gist@gmail.com>
2026-06-23 20:12:06 +0530

    DiscountCalculatorSystem

--- Day 15 - Eclipse/switch-block/src/com/hello/block/DiscountCalculator.java
+++ Day 15 - Eclipse/switch-block/src/com/hello/block/DiscountCalculator.java
@@ -3,8 +3,42 @@ package com.hello.block;
 public class DiscountCalculator {
 
 	public static void main(String[] args) {
-		
+
+		int customerType = Integer.parseInt(args[0]);
+		double billAmount = Double.parseDouble(args[1]);
+
+		calculateDiscount(customerType, billAmount);
+
 	}
-	private static void 
-	
+
+	private static void calculateDiscount(int customerType, double billAmount) {
+
+		double discount = 0;
+
+		switch (customerType) {
+
+		case 1:
+			discount = billAmount * 5d / 100d;
+			System.out.println("Customer type: Silver");
+			break;
+
+		case 2:
+			discount = billAmount * 10d / 100d;
+			System.out.println("Customer type: Gold");
+			break;
+
+		case 3:
+			discount = billAmount * 15d / 100d;
+			System.out.println("Customer type: Platinum");
+			break;
+
+		default:
+			System.out.println("Invalid Customer Type");
+			break;
+
+		}
+		System.out.println("Discount You got : " + discount);
+		System.out.println("Total Price after Discount: Rs." + (billAmount - discount));
+	}
+
 }
