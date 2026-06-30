9d0dd80110f7628c0345198fee28fe1cdd50a400
Sarvagay Sanmotra <sarvagay123gist@gmail.com>
2026-07-01 00:29:10 +0530

    Updated

--- Day 15 - Eclipse/oject-creation/src/com/sbi/user1/Account.java
+++ Day 15 - Eclipse/oject-creation/src/com/sbi/user1/Account.java
@@ -6,10 +6,12 @@ public class Account {
 	private int balance;
 	
 	Account(){
+		this("Sar@123", 10000 );
 		
 	}
 	
 	Account(String _name, int _balance){
+		super();
 		this.name = _name;
 		this.balance = _balance;
 		System.out.println(name);
